b65b9ecd36e8945fc4dd86206ab0621a6d11310d
Debashish <debashish.dash@ltts.com>
2021-06-29 18:58:22 +0530

    first commit

---
+++ first_prog.py
@@ -0,0 +1,19 @@
+ #simple echo server
+import socket
+import sys
+SERVER_IP="127.0.0.1"
+SERVER_PORT=1500
+MAXSIZE=64
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_sock.bind((SERVER_IP, SERVER_PORT))
+server_sock.listen(3)
+client, addr = server_sock.accept()
+print("Received a connection from", addr)
+while True:
+    data = client.recv(MAXSIZE)
+    if not data : 
+        break
+    print("client says :", data.decode('utf-8'))
+    client.send(data)
+    client.close()
+server_sock.close()

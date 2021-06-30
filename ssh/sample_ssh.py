02c0fdb29de5f8342fefde1e9c21b274d17db6e1
debasish2110 <debasishdash98@gmail.com>
2021-07-01 01:36:04 +0530

    ssh added and git user changed

---
+++ ssh/sample_ssh.py
@@ -0,0 +1,98 @@
+"""
+File: sample ssh test
+Owner: SYS
+Copyright: (c) LTTS
+"""
+#importing modules
+
+import os
+import sys
+import time
+import socket
+from ssh2.session import Session
+
+# CONSTANTS / GLOBALS
+# HOST = '192.168.29.200' #ip of raspberry pi of instructor
+HOST = 'localhost'
+PORT = 22
+USER = 'test_user'    # instructor set user name in the ubuntu 20.04 installed in raspberry pi
+#USER = os.getlogin()
+PASSWORD = 'Test321'
+
+# class defination of ssh channel creation
+class Ssh_session():
+    """
+        [summary: class definition]
+    """
+
+    _class_var = None
+
+    def __init__(self, host, user, port, password):
+        """
+        [summary: constructor]
+
+        Args:
+            host ([type]): [description]
+            user ([type]): [description]
+            port ([type]): [description]
+            password ([type]): [description]
+        """
+        self._host = host   #private variable
+        self._user = user
+        self._port = port
+        self._password = password
+        self.channel = None  #non private
+        self.session = None
+    # End of constructor
+
+    def get_channel(self):
+        """
+        [summary:]
+        """
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(self._host, self._port)
+
+        self.session = Session()
+        self.session.handshake(sock)
+        self.session.userauth_password(self._user, self._password)
+        self.channel = self.session.open_session()
+    # End of Function get_channel
+
+    def execute_cmd(self, cmd):
+        """[summary: executes remote command]
+
+        Args:
+            cmd ([type]): [description]
+        """
+        size = 0
+        data_list = []
+        self.channel.execute(cmd)
+        s, d = self.channel.read()
+        size += s
+        data_list.append(d)
+
+        while s > 0:
+            s, d = channel.read()
+            size += s
+            data_list.append(d)
+        return size, data_list
+    # End of function
+
+    def __del__(self):
+        """
+        [summary: Destroctor]
+        """
+        # self.channel.close()
+        print("Exit Status: %s" % self.channel.get_exit_ststus())
+    # End of destroctor
+# End of Class
+
+if __name__ == "__main__":
+    ssh_ses_obj = Ssh_session(HOST, USER, PORT, PASSWORD) #creating obj
+    ssh_ses_obj.get_channel() #creating the channel
+    # sending command
+    cmd = 'vmstat; ping -c 5 192.168.29.169; exit 0;'
+    size, data = ssh_ses_obj.execute_cmd(cmd)
+    ssh_ses_obj.channel.close()
+    print("Received Size: {} and Data: {}".format(size, data))
+    sys.exit(0)
---
+++ ssh/ssh_example.py
@@ -0,0 +1,23 @@
+# Example from the documentation file
+import os
+import socket
+from ssh2.session import Session
+
+host = 'localhost'
+user = os.getlogin()
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((host, 22))
+
+session = Session()
+session.handshake(sock)
+session.agent_auth(user)
+
+channel = session.open_session()
+channel.execute('echo me; exit 2')
+size, data = channel.read()
+while size > 0:
+    print(data)
+    size, data = channel.read()
+channel.close()
+print("Exit status: %s" % channel.get_exit_status())
